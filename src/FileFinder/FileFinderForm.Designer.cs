ec7825a0966c70c24fc4baa524d6fbd08b7666db
sandrapatfer <sandrapatfer@gmail.com>
2012-01-19 17:36:13 +0000

    update

--- src/FileFinder/FileFinderForm.Designer.cs
+++ src/FileFinder/FileFinderForm.Designer.cs
@@ -92,6 +92,7 @@
             this.btnCancel.TabIndex = 8;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // txtResults
             // 
--- src/FileFinder/FileFinderForm.cs
+++ src/FileFinder/FileFinderForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Threading;
 
 namespace FileFinder
 {
     public partial class FileFinderForm : Form
     {
+        private bool _canceled;
+        private int _count = 0;
         public FileFinderForm()
         {
             InitializeComponent();
@@ -41,15 +44,141 @@ namespace FileFinder
             }
 
             btnSearch.Enabled = false;
+            btnCancel.Enabled = true;
             txtResults.Text = "";
-            foreach (var fname in Directory.EnumerateFiles(fldr, patt, SearchOption.AllDirectories))
+            _canceled = false;
+/*            Thread t = new Thread(new ThreadStart(() =>
             {
-                String name = fname;
-                String content = File.ReadAllText(name);
-                if (content.Contains(word))
-                    txtResults.AddLine(name);
-            }
-            btnSearch.Enabled = true;
+                foreach (var fname in Directory.EnumerateFiles(fldr, patt, SearchOption.AllDirectories))
+                {
+                    if (_canceled)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        String name = fname;
+                        String content = File.ReadAllText(name);
+                        if (content.Contains(word))
+                            txtResults.AddLine(name);
+                    }
+                }
+            }));
+            t.Start();*/
+
+/*            ThreadPool.QueueUserWorkItem( new WaitCallback( (o)=>
+            {
+                foreach (var fname in Directory.EnumerateFiles(fldr, patt, SearchOption.AllDirectories))
+                {
+                    if (_canceled)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        String name = fname;
+                        ThreadPool.QueueUserWorkItem(new WaitCallback((o2) =>
+                            {
+                                if (!_canceled)
+                                {
+                                    String content = File.ReadAllText(name);
+                                    if (content.Contains(word))
+                                        txtResults.AddLine(name);
+                                }
+                            }));
+                    }
+                }
+            }));*/
+
+/*            _count = 0;
+            int count_active_work_items = 1;
+            ThreadPool.QueueUserWorkItem( (o1)=>
+            {
+                foreach (var dname in Directory.EnumerateDirectories(fldr, "*", SearchOption.AllDirectories))
+                {
+                    if (!_canceled)
+                    {
+                        Interlocked.Increment(ref count_active_work_items);
+                        ThreadPool.QueueUserWorkItem((o2) =>
+                            {
+                                if (!_canceled)
+                                {
+                                    foreach (var fname in Directory.EnumerateFiles(dname, patt))
+                                    {
+                                        if (_canceled)
+                                        {
+                                            break;
+                                        }
+                                        String name = fname;
+                                        Interlocked.Increment(ref _count);
+                                        String content = File.ReadAllText(name);
+                                        if (content.Contains(word))
+                                            txtResults.AddLine(name);
+                                    }
+                                    if (Interlocked.Decrement(ref count_active_work_items) == 0)
+                                    {
+                                        this.BeginInvoke(new Action(() =>
+                                        {
+                                            btnCancel.Enabled = false;
+                                            btnSearch.Enabled = true;
+                                            MessageBox.Show(string.Format("Finished", _count));
+                                        }));
+                                    }
+                                }
+                            });
+                    }
+                }
+                if (Interlocked.Decrement(ref count_active_work_items) == 0)
+                {
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        btnCancel.Enabled = false;
+                        btnSearch.Enabled = true;
+                        MessageBox.Show(string.Format("Finished", _count));
+                    }));
+                }
+            });*/
+
+            _count = 0;
+            var itemsCounter = new ActiveWorkItemCounter(new Action(() =>
+                                        {
+                                            this.BeginInvoke(new Action(() =>
+                                                {
+                                                    btnCancel.Enabled = false;
+                                                    btnSearch.Enabled = true;
+                                                    MessageBox.Show(string.Format("Finished", _count));
+                                                }));
+                                        }));
+            ThreadPool.QueueUserWorkItem( (o1)=>
+            {
+                foreach (var dname in Directory.EnumerateDirectories(fldr, "*", SearchOption.AllDirectories))
+                {
+                    if (!_canceled)
+                    {
+                        itemsCounter.Increment();
+                        ThreadPool.QueueUserWorkItem((o2) =>
+                            {
+                                if (!_canceled)
+                                {
+                                    foreach (var fname in Directory.EnumerateFiles(dname, patt))
+                                    {
+                                        if (_canceled)
+                                        {
+                                            break;
+                                        }
+                                        String name = fname;
+                                        Interlocked.Increment(ref _count);
+                                        String content = File.ReadAllText(name);
+                                        if (content.Contains(word))
+                                            txtResults.AddLine(name);
+                                    }
+                                    itemsCounter.Decrement();
+                                }
+                            });
+                    }
+                }
+                itemsCounter.Decrement();
+            });
         }
 
         private void btnFolder_Click(object sender, EventArgs e)
@@ -62,14 +191,25 @@ namespace FileFinder
             }
  
         }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _canceled = true;
+            btnCancel.Enabled = false;
+            btnSearch.Enabled = true;
+            MessageBox.Show(string.Format("Processed {0} files", _count));
+        }
     }
 
     static class TextBoxExtensions
     {
         public static TextBox AddLine(this TextBox textBox, String text)
         {
-            textBox.AppendText(text);
-            textBox.AppendText("\r\n");
+            textBox.BeginInvoke(new Action(() =>
+                {
+                    textBox.AppendText(text);
+                    textBox.AppendText("\r\n");
+                }));
             return textBox;
         }
     }
